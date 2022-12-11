f77f77f519749825b473da28ac4d29e55e3a17c9
Carlos Gomez <carlosandres0741@gmail.com>
2022-12-10 22:48:27 -0500

    Fix: request limit

--- src/components/CardRecipe.jsx
+++ src/components/CardRecipe.jsx
@@ -1,26 +1,10 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 
 const CardRecipe = ({ recipe }) => {
-  // const [recipeInformation, setRecipeInformation] = useState({})
-
-  // useEffect(() => {
-  //   axios.get(`https://api.spoonacular.com/recipes/${recipe.id}/information`, {
-  //     params: {
-  //       apiKey: import.meta.env.VITE_API_KEY
-  //     }
-  //   }).then(response => {
-  //     setRecipeInformation(response.data)
-  //   })
-  // }, [])
-
   return (
     <li>
       <h3>{recipe.title}</h3>
       <img alt={recipe.title} src={recipe.image} />
-      {/* <p>{recipeInformation.vegan ? 'Vegan' : 'No vegan'}</p> */}
-      {/* <p>Preparation minutes: {recipeInformation.preparationMinutes}</p> */}
-      {/* <p>Cooking minutes: {recipeInformation.cookingMinutes}</p> */}
     </li>
   )
 }
